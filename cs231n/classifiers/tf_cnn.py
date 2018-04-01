bcd0ddac5bde29d205b8fba53e306c2939385b6e
Christine <allec@seas.upenn.edu>
2018-04-01 14:06:50 -0400

    removed import builtins, python2 doesnt like it

--- cs231n/classifiers/tf_cnn.py
+++ cs231n/classifiers/tf_cnn.py
@@ -4,7 +4,7 @@ import argparse
 import tensorflow as tf
 import numpy as np
 
-from cs231n.data_utils import load_CIFAR10
+from cs231n.data_utils import get_CIFAR10_data
 
         
 class Baseline(object):
@@ -204,37 +204,6 @@ class ConvModelAbs(Baseline):
         return a
 
     
-def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
-    """
-    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
-    it for the two-layer neural net classifier. These are the same steps as
-    we used for the SVM, but condensed to a single function.  
-    """
-    # Load the raw CIFAR-10 data
-    cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
-    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
-
-    # Subsample the data
-    mask = range(num_training, num_training + num_validation)
-    X_val = X_train[mask]
-    y_val = y_train[mask]
-    mask = range(num_training)
-    X_train = X_train[mask]
-    y_train = y_train[mask]
-    mask = range(num_test)
-    X_test = X_test[mask]
-    y_test = y_test[mask]
-
-    # Normalize the data: subtract the mean image
-    mean_image = np.mean(X_train, axis=0)
-    X_train -= mean_image
-    X_val -= mean_image
-    X_test -= mean_image
-
-    return X_train, y_train, X_val, y_val, X_test, y_test
-
-
-
             
 if __name__ == "__main__":
     models = {'relu':Baseline, 'max_norm':ConvModelMaxNorm, 'abs':ConvModelAbs}
@@ -250,18 +219,19 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Invoke the above function to get our data.
-    X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()
-    print('Train data shape: ', X_train.shape)
-    print('Train labels shape: ', y_train.shape)
-    print('Validation data shape: ', X_val.shape)
-    print('Validation labels shape: ', y_val.shape)
-    print('Test data shape: ', X_test.shape)
-    print('Test labels shape: ', y_test.shape)
+    # X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()
+    data = get_CIFAR10_data()
+    # print('Train data shape: ', data['X_train'].shape)
+    # print('Train labels shape: ', data['y_train'].shape)
+    # print('Validation data shape: ', data['X_val'].shape)
+    # print('Validation labels shape: ', data['y_val'].shape)
+    # print('Test data shape: ', data['X_test'].shape)
+    # print('Test labels shape: ', data['y_test'].shape)
 
     network_name = args.network
     logdir = 'graphs/' + network_name
     savedir = 'checkpoints/' + network_name
     
     my_model = models[network_name](logdir, savedir)
-    my_model.setup(X_train, y_train, num_epochs=args.epochs, batch_size=args.batch_size)
+    my_model.setup(data['X_train'], data['y_train'], num_epochs=args.epochs, batch_size=args.batch_size)
     my_model.train(print_every=args.print_every, save_every=args.save_every)
